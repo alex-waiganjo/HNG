2fb578a251eb5c19fe2eae8bb94c078614d78440
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-09 19:15:03 +0300

    extra

--- app.py
+++ app.py
@@ -4,36 +4,37 @@ import datetime
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
-@app.route('/')
-def show():
-    date = datetime.datetime.now()
-    github_repo = 'https://github.com/alex-waiganjo/HNG'
-    details = {
-        "Slack_name": "slack_name",
-        "Current_day": date.strftime('%A'),
-        "Utc_time": date.isoformat(),
-        "Track": "testingtrack",
-        "Github_file_url": github_repo,
-        "Github_repo_url": github_repo,
-        "Status_code": 200
-    }
-    return details
-
-# Stage 2 Task {Host an API with your HNG Details }
-# @app.route("/api/<slack_name>/<track>")
-# def My_Profile(slack_name, track):
+# @app.route('/')
+# def show():
 #     date = datetime.datetime.now()
 #     github_repo = 'https://github.com/alex-waiganjo/HNG'
 #     details = {
-#         "Slack_name": slack_name,
+#         "Slack_name": "slack_name",
 #         "Current_day": date.strftime('%A'),
 #         "Utc_time": date.isoformat(),
-#         "Track": track,
+#         "Track": "testingtrack",
 #         "Github_file_url": github_repo,
 #         "Github_repo_url": github_repo,
 #         "Status_code": 200
 #     }
 #     return details
+
+# Stage 2 Task {Host an API with your HNG Details }
+@app.route('/')
+@app.route("/api/<slack_name>/<track>")
+def My_Profile(slack_name, track):
+    date = datetime.datetime.now()
+    github_repo = 'https://github.com/alex-waiganjo/HNG'
+    details = {
+        "Slack_name": slack_name,
+        "Current_day": date.strftime('%A'),
+        "Utc_time": date.isoformat(),
+        "Track": track,
+        "Github_file_url": github_repo,
+        "Github_repo_url": github_repo,
+        "Status_code": 200
+    }
+    return details
 
 
 if __name__ == '__main__':
