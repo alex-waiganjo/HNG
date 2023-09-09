8d7ecf3cff88bacc74a1d454122849f52a01848b
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-09 21:35:59 +0300

    Testing

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 import datetime
-
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
-# @app.route('/')
-# def show():
-#     date = datetime.datetime.now()
-#     github_repo = 'https://github.com/alex-waiganjo/HNG'
-#     details = {
-#         "Slack_name": "slack_name",
-#         "Current_day": date.strftime('%A'),
-#         "Utc_time": date.isoformat(),
-#         "Track": "testingtrack",
-#         "Github_file_url": github_repo,
-#         "Github_repo_url": github_repo,
-#         "Status_code": 200
-#     }
-#     return details
 
 # Stage 2 Task {Host an API with your HNG Details }
-@app.route('/')
-@app.route("/api/<slack_name>/<track>")
-def My_Profile(slack_name, track):
+# @app.route('/',)
+@app.route("/api",methods=['GET'])
+def My_Profile():
+    slack_name = request.args.get('slack_name')
+    track = request.args.get('track')
     date = datetime.datetime.now()
     github_repo = 'https://github.com/alex-waiganjo/HNG'
     details = {
