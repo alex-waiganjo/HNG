7b772dd172acede660fa5bbe9ba348fdf7c02a4e
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-09 18:11:33 +0300

    Stage 2

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import datetime
+
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+# Stage 2 Task {Host an API with your HNG Details }
+@app.route("/api/<slack_name>/<track>")
+def My_Profile(slack_name, track):
+    date = datetime.datetime.now()
+    github_repo = 'https://github.com/alex-waiganjo/HNG'
+    details = {
+        "Slack_name": slack_name,
+        "Current_day": date.strftime('%A'),
+        "Utc_time": date.isoformat(),
+        "Track": track,
+        "Github_file_url": github_repo,
+        "Github_repo_url": github_repo,
+        "Status_code": 200
+    }
+    return details
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
